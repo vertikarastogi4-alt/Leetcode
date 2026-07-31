00fde1c2e11a1a2c9a3a3a980ac48e60422ca01e
vertikarastogi4-alt <vertikarastogi4@gmail.com>
2026-07-31 21:41:21 +0530

    Time: 2 ms (50.75%) | Memory: 8.6 MB (65.68%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+         if (x < 0)
+            return false;
+
+        int original = x;
+        long long reverse = 0;
+
+        while (x > 0) {
+            reverse = reverse * 10 + x % 10;
+            x /= 10;
+        }
+
+        return original == reverse;
+    }
+};
+        
+    

4c147644d23e15426f57811755219c2b56a241f5
vertikarastogi4-alt <vertikarastogi4@gmail.com>
2026-08-05 22:37:51 +0530

    Time: 38 ms (75.36%) | Memory: 45.5 MB (55.95%) - LeetSync

---
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        unordered_map<int, int> mpp;
+        mpp[0] = 1;
+
+        int preSum = 0;
+        int cnt = 0;
+
+        for (int i = 0; i < nums.size(); i++) {
+            preSum += nums[i];
+
+            if (mpp.find(preSum - k) != mpp.end())
+                cnt += mpp[preSum - k];
+
+            mpp[preSum]++;
+        }
+
+        return cnt;
+    }
+};

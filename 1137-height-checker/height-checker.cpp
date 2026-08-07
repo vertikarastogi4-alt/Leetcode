d490b9f6ff3ba577a53dbc7307acec178ee0c6a2
vertikarastogi4-alt <vertikarastogi4@gmail.com>
2026-08-07 23:38:40 +0530

    Time: 0 ms (100.00%) | Memory: 11.5 MB (5.24%) - LeetSync

---
+++ 1137-height-checker/height-checker.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int heightChecker(vector<int>& heights) {
+        vector<int> sorted = heights;
+
+        sort(sorted.begin(), sorted.end());
+
+        int cnt = 0;
+
+        for (int i = 0; i < heights.size(); i++) {
+            if (heights[i] != sorted[i]) {
+                cnt++;
+            }
+        }
+
+        return cnt;
+    }
+};

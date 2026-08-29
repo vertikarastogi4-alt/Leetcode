547fcf1424fa41c8eebf8da4df6dd537efa552f2
vertikarastogi4-alt <vertikarastogi4@gmail.com>
2026-08-29 23:44:05 +0530

    Time: 0 ms (100.00%) | Memory: 95.2 MB (61.29%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,31 +1,76 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        vector<int> nums3;
 
         int n1 = nums1.size();
         int n2 = nums2.size();
 
+        int n = n1 + n2;
+
+        int ind2 = n / 2;
+        int ind1 = ind2 - 1;
+
         int i = 0;
         int j = 0;
+        int cnt = 0;
+
+        int ind1el = -1;
+        int ind2el = -1;
+
         while (i < n1 && j < n2) {
+
             if (nums1[i] < nums2[j]) {
-                nums3.push_back(nums1[i++]);
-            } 
+
+                if (cnt == ind1)
+                    ind1el = nums1[i];
+
+                if (cnt == ind2)
+                    ind2el = nums1[i];
+
+                cnt++;
+                i++;
+            }
             else {
-                nums3.push_back(nums2[j++]);
+
+                if (cnt == ind1)
+                    ind1el = nums2[j];
+
+                if (cnt == ind2)
+                    ind2el = nums2[j];
+
+                cnt++;
+                j++;
             }
         }
+
         while (i < n1) {
-            nums3.push_back(nums1[i++]);
+
+            if (cnt == ind1)
+                ind1el = nums1[i];
+
+            if (cnt == ind2)
+                ind2el = nums1[i];
+
+            cnt++;
+            i++;
         }
+
         while (j < n2) {
-            nums3.push_back(nums2[j++]);
+
+            if (cnt == ind1)
+                ind1el = nums2[j];
+
+            if (cnt == ind2)
+                ind2el = nums2[j];
+
+            cnt++;
+            j++;
         }
-        int n = nums3.size();
+
         if (n % 2 == 1) {
-            return nums3[n / 2];
+            return ind2el;
         }
-        return (nums3[n / 2] + nums3[n / 2 - 1]) / 2.0;
+
+        return (double)(ind1el + ind2el) / 2.0;
     }
 };

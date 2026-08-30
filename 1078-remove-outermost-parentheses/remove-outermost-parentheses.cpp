e0bfbff59807a74b9649334187aa42c089b699bf
Vertika Rastogi <vertikarastogi4@gmail.com>
2026-08-30 21:39:45 +0530

    Time: 0 ms (100.00%) | Memory: 8.8 MB (81.15%) - LeetSync

--- 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
+++ 1078-remove-outermost-parentheses/remove-outermost-parentheses.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     string removeOuterParentheses(string s) {
         string ans="";
-        stack<char> st;
+        int cnt=0;
         for( char ch : s)
         {
-        if(ch == ')') st.pop(); 
-            if(!(st.empty())) ans+=ch;
-            if( ch=='(') st.push(ch);
+        if(ch == ')') cnt--;
+            if(cnt>0) ans+=ch;
+            if( ch=='(') cnt++;
 
         }
         return ans;

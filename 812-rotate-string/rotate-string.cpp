3484a5ce595059fb320df461fa418a4f3ca47bd4
Vertika Rastogi <vertikarastogi4@gmail.com>
2026-09-03 16:41:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (97.31%) - LeetSync

--- 812-rotate-string/rotate-string.cpp
+++ 812-rotate-string/rotate-string.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     bool rotateString(string s, string goal) {
-     return s.size()==goal.size() && (s + s).find(goal)!= string::npos;   
+        if (s.size() != goal.size())
+            return false;
+
+        int n = s.size();
+
+        for (int i = 0; i < n; i++) {
+            bool match = true;
+
+            for (int j = 0; j < n; j++) {
+                if (s[(i + j) % n] != goal[j]) {
+                    match = false;
+                    break;
+                }
+            }
+
+            if (match)
+                return true;
+        }
+
+        return false;
     }
 };

6b8b2097e7f9d7553a9011ffab8b89b77713677f
Vertika Rastogi <vertikarastogi4@gmail.com>
2026-09-05 16:09:57 +0530

    Time: 1 ms (54.10%) | Memory: 9.9 MB (25.74%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,14 +1,23 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if(s.length()!=t.length())
-        return false;
-       
-        sort(s.begin(), s.end());
-        sort(t.begin(),t.end());
-        return s==t;
-        
+        if (s.length() != t.length())
+            return false;
+
+        unordered_map<char, int> frq;
+
+        for (char ch : s) {
+            frq[ch]++;
+        }
+        for (char ch : t) {
+            frq[ch]--;
+
+            if (frq[ch] < 0)
+                return false;
+        }
+
+        return true;
     }
+};
    
 
-};

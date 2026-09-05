1003dbd812bf820e78d0fa74f0433ba7926c9bfa
Vertika Rastogi <vertikarastogi4@gmail.com>
2026-09-05 23:38:05 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (7.54%) - LeetSync

--- 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
+++ 1737-maximum-nesting-depth-of-the-parentheses/maximum-nesting-depth-of-the-parentheses.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int maxDepth(string s) {
         int ans=0, n=0;
-        stack<char>st;
+      
         for(auto ch:s)
         {
             if ( ch=='(') 
             {
-                st.push(ch);
+                n++;
             }
             else if( ch==')')
             {
-                st.pop();
+                n--;
             }
-            n=st.size();
+        
             ans=ans<n?n:ans;
         }
         return ans;
